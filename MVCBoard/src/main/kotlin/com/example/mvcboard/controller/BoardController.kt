071641cd55aeea7da3a282355cc9696fac2c707b
sanghyeonKim0201 <gmhty4345@gmail.com>
2023-03-04 15:53:40 +0900

    setting

--- MVCBoard/src/main/kotlin/com/example/mvcboard/controller/BoardController.kt
+++ MVCBoard/src/main/kotlin/com/example/mvcboard/controller/BoardController.kt
@@ -1,22 +1,32 @@
 package com.example.mvcboard.controller
 
+import com.example.mvcboard.dto.BoardDto
+import com.example.mvcboard.service.BoardService
+
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("board")
-class BoardController {
+class BoardController(@Autowired private var boardService: BoardService) {
 
     @GetMapping("/")
     fun list(): String {
-        println("wasd")
         return "board/list"
     }
 
     @GetMapping("/post")
     fun post(): String {
-        println("post")
         return "board/post"
     }
+
+    @PostMapping("/post")
+    fun write(boardDto: BoardDto): String{
+        boardService.savePost(boardDto)
+        return "redirect:/"
+    }
+
 }
--- MVCBoard/src/main/kotlin/com/example/mvcboard/service/BoardService.kt
+++ MVCBoard/src/main/kotlin/com/example/mvcboard/service/BoardService.kt
@@ -1,4 +1,17 @@
 package com.example.mvcboard.service
 
-class BoardService {
+import com.example.mvcboard.domain.Repository.BoardRepository
+import com.example.mvcboard.domain.entity.Board
+import com.example.mvcboard.dto.BoardDto
+import jakarta.transaction.Transactional
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class BoardService(private var boardRepository: BoardRepository) { //생성자가 하나일 경우 @Autowired 생략 가능
+
+    fun savePost(boardDto: BoardDto) : Long{
+        return boardRepository.save(boardDto.toEntity()).id
+    }
+
 }
